cd2ed566b47c1bb7b11b1c0f81cb5f67a4eea7cb
krzysztofdanda <dandzior84@gmail.com>
2024-11-24 15:10:30 +0100

    login koniec

--- src/views/Login.js
+++ src/views/Login.js
@@ -3,13 +3,17 @@ import './Login.css';
 import { useState } from 'react';
 
 import axios from "axios";
+import { Navigate } from 'react-router-dom';
 
-const Login = () => {
+const Login = (props) => {
 
     const [formData, setFormData] = useState({
         username: "",
         password: "",
     });
+
+
+    const [loginMessage, setLoginMessage] = useState('')
 
     const handleInputChange = (e) => {
         const target = e.target;
@@ -31,7 +35,17 @@ const Login = () => {
         })
             .then((res) => {
 
-                console.log(res.data)
+                if (Array.isArray(res.data.username)) {
+                    setLoginMessage(res.data.username[0])
+                } else if (Array.isArray(res.data.password)) {
+                    setLoginMessage(res.data.password[0])
+                } else if (res.data.error) {
+                    setLoginMessage('Incorrect username or password')
+                } else {
+                    setLoginMessage("");
+                    props.setUser(res.data);
+                    localStorage.setItem('user', JSON.stringify(res.data));
+                }
             })
             .catch((error) => {
                 console.error(error);
@@ -40,7 +54,9 @@ const Login = () => {
 
     return (
         <div className="login">
+            {props.user && <Navigate to="/" />}
             <form onSubmit={handleSubmit}>
+                {loginMessage && <h2>{loginMessage}</h2>}
                 <input type="text" name="username" placeholder="User name" value={formData.username} onChange={handleInputChange} />
                 <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleInputChange} />
                 <button className="btn">Login</button>
