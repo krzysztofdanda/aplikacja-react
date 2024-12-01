8651413aa9a99715988e265192e74c42bbd529e7
krzysztofdanda <dandzior84@gmail.com>
2024-12-01 14:58:15 +0100

    AddPostkoniec

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,9 +1,41 @@
+import './AddPost.css';
+
+import { useState } from 'react';
+
+import axios from 'axios';
+
 const AddPost = (props) => {
-return <form>
-    <textarea placeholder="Add post..."></textarea>
-    <button className="btn">Add</button>
-</form>
-}
+
+    const [postContent, setPostContent] = useState('');
+
+    const AddPost = (e) => {
+        e.preventDefault();
+
+        if (!postContent) {
+            return;
+        }
+
+
+        axios.post("https://akademia108.pl/api/social-app/post/add", {
+            content: postContent
+        })
+            .then(() => {
+                props.getPrevPosts();
+                setPostContent('');
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+
+        return (
+        <form className="addPostForm" onSubmit={AddPost}>
+            <textarea placeholder="Add post..." onChange={(e) => setPostContent(e.target.value)} value={postContent}></textarea>
+            <button className="btn">Add</button>
+        </form>
+       );
+    
+};
+};
 
 
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,6 +3,7 @@ import './Home.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Post from "../components/Post";
+import AddPost from '../components/AddPost';
 
 const Home = (props) => {
     const [posts, setPosts] = useState([])
@@ -24,11 +25,27 @@ const Home = (props) => {
     const getNextPosts = () => {
 
         axios.post("https://akademia108.pl/api/social-app/post/older-then", {
-            date: posts[posts.length - 1].created_at
+            date: posts[posts.length - 1].created_at,
         })
             .then((res) => {
 
-                setPosts(posts.concat(res.data));
+                setPosts(posts.concat(res.data))
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+
+    };
+
+    const getPrevPosts = () => {
+
+        axios.post("https://akademia108.pl/api/social-app/post/newer-then", {
+            date: posts[0].created_at,
+        })
+            .then((res) => {
+
+                setPosts(res.data.concat(posts))
             })
             .catch((error) => {
                 console.error(error);
@@ -43,6 +60,7 @@ const Home = (props) => {
 
     return (
         <div className="home">
+            {props.user && <AddPost getPrevPosts={getPrevPosts} />}
             <div className="postList">{posts.map((post) => {
                 return <Post post={post} key={post.id} />;
             })}
