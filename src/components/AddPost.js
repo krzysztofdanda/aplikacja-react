a78f8a24fe3b7603b1ee8e60570dfbff89703b30
krzysztofdanda <dandzior84@gmail.com>
2024-12-01 13:50:25 +0100

    dodawanie-postow-add

---
+++ src/components/AddPost.js
@@ -0,0 +1,10 @@
+const AddPost = (props) => {
+return <form>
+    <textarea placeholder="Add post..."></textarea>
+    <button className="btn">Add</button>
+</form>
+}
+
+
+
+export default AddPost;
